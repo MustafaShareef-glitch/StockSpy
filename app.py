91e66872341432f44df47e997817e0eee4d779dc
Mustafa <smohammedmustafa9@gmail.com>
2025-06-13 19:05:28 +0530

    Added stock chart using Plotly

--- app.py
+++ app.py
@@ -1,37 +1,25 @@
-
-
-
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import yfinance as yf
 import plotly.graph_objs as go
-import plotly.io as pio
+import plotly.offline as pyo
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    price_info = ""
-    chart_html = ""
-
+def index():
+    chart_html = None
     if request.method == 'POST':
-        symbol = request.form.get('symbol').upper()
-        try:
-            ticker = yf.Ticker(symbol)
-            data = ticker.history(period="7d")
-            if not data.empty:
-                latest_price = round(data['Close'][-1], 2)
-                price_info = f"{symbol} Stock Price: ${latest_price}"
+        symbol = request.form['symbol'].upper()
+        data = yf.download(symbol, period='7d', interval='1d')
+        
+        if not data.empty:
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines+markers', name='Close Price'))
+            fig.update_layout(title=f'{symbol} Price - Last 7 Days', xaxis_title='Date', yaxis_title='Price')
+            chart_html = pyo.plot(fig, output_type='div')
 
-                fig = go.Figure()
-                fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name=symbol))
-                fig.update_layout(title=f"{symbol} - Last 7 Days", xaxis_title='Date', yaxis_title='Price ($)')
-                chart_html = pio.to_html(fig, full_html=False)
-            else:
-                price_info = "No data available for this symbol."
-        except:
-            price_info = "Error fetching data. Please try again."
-
-    return render_template("home.html", price_info=price_info, chart_html=chart_html)
+    return render_template('index.html', chart_html=chart_html)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
