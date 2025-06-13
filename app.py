0c59a71af142058a49ddbc3940068d3ed06b8f77
Mustafa <smohammedmustafa9@gmail.com>
2025-06-13 17:37:56 +0530

    Display live Apple stock price using yfinance

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import yfinance as yf
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, StockSpy is working!"
+    ticker = yf.Ticker("AAPL")  # Apple stock
+    data = ticker.history(period="1d")
+    price = round(data['Close'][0], 2)
+    return f"Apple Stock Price: ${price}"
